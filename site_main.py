4d7bb7acf63b461f271e7f3ebd4764beaeb852b0
naliuj <julianrose@verizon.net>
2016-04-27 12:28:35 -0400

    nav bar is now part of the base.html template

--- site_main.py
+++ site_main.py
@@ -5,6 +5,7 @@ import db
 import tweepy
 from twitter_auth import authenticate
 from CONFIG import SECRET_KEY
+import login_db
 
 
 app = Flask(__name__)
@@ -24,12 +25,25 @@ def login_required(f):
     return wrap
 
 
+# Decorator to lock log in page after logged in
+def no_login(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "logged_in" not in session:
+            return f(*args, **kwargs)
+        else:
+            flash("logged in")
+            return redirect(url_for("schedule"))
+    return wrap
+
+
 @app.route("/")
 def index():
-    return render_template("index.html", subheading="Home")
+    return redirect(url_for("login"))
 
 
 @app.route("/login/", methods=["GET", "POST"])
+@no_login
 def login():
     error = None
     if request.method == "POST":
@@ -39,21 +53,20 @@ def login():
             session["logged_in"] = True
             flash("You were just logged in!")
             return redirect(url_for("index"))
-    return render_template("login.html", subheading="Log In", error=error)
+    return render_template("login.html", subheading="Log In", error=error, page="login")
 
 
 @app.route("/logout/")
 @login_required
 def logout():
     session.pop("logged_in", None)
-    flash("You were just logged out!")
     return redirect(url_for("index"))
 
 
 @app.route("/schedule/")
 @login_required
 def schedule():
-    return render_template("schedule.html", shows=get_table(), slots=db.read(), subheading="Schedule")
+    return render_template("schedule.html", shows=get_table(), slots=db.read(), subheading="Schedule", page="schedule")
 
 
 @app.route("/add/", methods=["GET", "POST"])
@@ -72,7 +85,7 @@ def add_show():
                      ]
         db.add(show_info)
 
-    return render_template("add_show.html", alert=None, subheading="Adding New Show")
+    return render_template("add_show.html", alert=None, subheading="Adding New Show", page="add")
 
 
 @app.route("/remove/", methods=["POST"])
@@ -89,7 +102,7 @@ def remove_show():
 @app.route("/manual-tweet/")
 @login_required
 def manual_tweet_page():
-    return render_template("manual_tweet.html", subheading="Send Tweet Manually", status=None)
+    return render_template("manual_tweet.html", subheading="Send Tweet Manually", status=None, page="manual-tweet")
 
 
 @app.route("/manual-tweet/", methods=["POST"])
@@ -99,15 +112,15 @@ def send_tweet():
     tweet = request.form["message"]
     try:
         twit.update_status(tweet)
-        return render_template("manual_tweet.html", subheading="Send Tweet Manually", status="success")
+        return render_template("manual_tweet.html", subheading="Send Tweet Manually", status="success", page="manual-tweet")
     except tweepy.error.TweepError:
-        return render_template("manual_tweet.html", subheading="Send Tweet Manually", status="failed")
+        return render_template("manual_tweet.html", subheading="Send Tweet Manually", status="failed", page="manual-tweet")
 
 
 @app.errorhandler(404)
 def page_note_found(e):
     print(e)
-    return render_template("error.html", subheading=e, error=e), 404
+    return render_template("error.html", subheading=e, error=e, page="error"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
